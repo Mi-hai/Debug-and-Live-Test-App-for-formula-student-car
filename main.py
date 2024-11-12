79a10acc0a9bffd4fbee936ad625dfb4578418d1
Mi-hai <mabargaoanu@gmail.com>
2024-11-12 21:17:46 +0200

    V0.2.1

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import os
 
 # Initialize the main window
 root = ctk.CTk()
-root.title("EVR app v0.2")
+root.title("EVR app")
 root.geometry("1920x1080")
 root.resizable(False, False)
+
 
 # Set up the appearance and theme for the GUI
 ctk.set_appearance_mode('dark')
@@ -26,7 +27,7 @@ def setup_input_section(root):
     label1.place(x=800, y=750)
     
     # Create an input field (CTkEntry)
-    input_entry = ctk.CTkEntry(master=root, placeholder_text="/dev/tty...", border_width=1, border_color="black")
+    input_entry = ctk.CTkEntry(root, placeholder_text="/dev/tty...", border_width=1, border_color="black")
     input_entry.place(x=865, y=790)  # Adds padding around the input field
 
     def retrieve_input():
@@ -34,7 +35,7 @@ def setup_input_section(root):
         input = input_entry.get().strip()  # Retrieve the text from the input field
         input_entry.delete(0, "end")
 
-    submit_button = ctk.CTkButton(master=root, text="Submit", command=retrieve_input, border_width=1, border_color="black")
+    submit_button = ctk.CTkButton(root, text="Submit", command=retrieve_input, border_width=1, border_color="black")
     submit_button.place(x=865, y=830)
 
 
@@ -51,6 +52,7 @@ text_button_live_test.place(x=1235, y=550)
 # Background Image for the GUI
 script_dir = os.path.dirname(os.path.abspath(__file__))  # Get the directory where this script is located
 image_path = os.path.join(script_dir, "assets", "logo.jpeg")  # Construct the image path
+
 my_image = ctk.CTkImage(dark_image=Image.open(image_path), size=(1920, 1000))
 image_label = ctk.CTkLabel(root, image=my_image, text="")
 image_label.place(x=0, y=-40, relwidth=1, relheight=1)
